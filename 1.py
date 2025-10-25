bf95057d54b72bad877a6c638f6ca3fcaa5b6500
abolfazl kholosi <156130284+Abol-khls@users.noreply.github.com>
2025-10-25 22:08:31 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -17,30 +17,19 @@ btn.click()
 
 inputEmail=driver.find_elements(By.XPATH,"/html/body/div[1]/div/div[2]/div[2]/transition/div/div[2]/div/div/div/div[2]/div/div[2]/form/div/div[1]/div/input")
 txt=inputEmail[0]
-txt.send_keys("zahramobasherai@gmail.com")
+txt.send_keys("")# Enter Your Email
 
 inputUsername=driver.find_elements(By.XPATH,"/html/body/div[1]/div/div[2]/div[2]/transition/div/div[2]/div/div/div/div[2]/div/div[2]/form/div/div[2]/div/input")
 txt=inputUsername[0]
-txt.send_keys("zmobasher")
+txt.send_keys("")# Enter Your Username
 
 inputPass=driver.find_elements(By.XPATH,"/html/body/div[1]/div/div[2]/div[2]/transition/div/div[2]/div/div/div/div[2]/div/div[2]/form/div/div[3]/div/input")
 txt=inputPass[0]
-txt.send_keys("123123")
+txt.send_keys("")#Enter Your Password
 
 submit=driver.find_elements(By.XPATH,"/html/body/div[1]/div/div[2]/div[2]/transition/div/div[2]/div/div/div/div[2]/div/div[2]/form/div/div[4]/div/button")
 btn=submit[0]
 btn.click()
-
-
-
-# btns=dv.find_elements(By.TAG_NAME,"img")
-# btn=btns[0]
-# btn.click()
-# inputs=driver.find_elements(By.ID,"main-search")
-# txt=inputs[0]
-# txt.send_keys("بارسا")
-# time.sleep(5)
-
 
 
 time.sleep(100)
